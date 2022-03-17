c16fb8198f84d2675d63e28dd445883e01b54626
ohick <oliviahick1@gmail.com>
2022-03-17 13:56:10 +0100

    fix login redirection

--- client/src/App.js
+++ client/src/App.js
@@ -26,9 +26,11 @@ function App() {
   useEffect(() => {
     const checkSession = async () => {
       const session = await validateSession(dispatch);
+      console.log(!session?.id)
       if (!session?.id) {
-        navigate('/login');
+        return navigate('/login');
       }
+      return null;
     };
 
     if (!authState.id) {
@@ -39,23 +41,32 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Routes>
-        <Route element={<WithoutNav />}>
-          <Route path="/login" element={<Login />} />
-        </Route>
-        <Route element={<WithNav />}>
-          <Route exact path="/my-playlists" element={<Playlists />} />
-          <Route exact path="/my-playlists/:id" element={<Playlist />} />
-          <Route exact path="/my-playlists/:id/edit" element={<EditPlaylist />} />
-          <Route exact path="/my-playlists/:id/delete" element={<DeletePlaylist />} />
-          <Route path="/create-playlist" element={<CreatePlaylist />} />
-          <Route path="/logout" element={<Logout />} />
-          <Route path="*" element={<NotFound />} />
-        </Route>
-      </Routes>
+      {authState.id ? (
+        <Routes>
+          <Route element={<WithoutNav />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+          <Route element={<WithNav />}>
+            <Route exact path="/my-playlists" element={<Playlists />} />
+            <Route exact path="/my-playlists/:id" element={<Playlist />} />
+            <Route exact path="/my-playlists/:id/edit" element={<EditPlaylist />} />
+            <Route exact path="/my-playlists/:id/delete" element={<DeletePlaylist />} />
+            <Route path="/create-playlist" element={<CreatePlaylist />} />
+            <Route path="/logout" element={<Logout />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
+      ) : (
+        <Routes>
+          <Route element={<WithoutNav />}>
+            <Route path="/login" element={<Login />} />
+          </Route>
+        </Routes>
+      )
+      }
+
     </ThemeProvider>
-  );
+  )
 }
 
 export default App;
-//
